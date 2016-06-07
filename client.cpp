edc2148749c9858e5784e05e75118c76e474f572
Kenneth Lee <kl488@cornell.edu>
2016-06-07 13:25:10 -0400

    Use name publishing and lookup

--- client.cpp
+++ client.cpp
@@ -9,18 +9,11 @@ using namespace std;
 
 int main (int argc, char* argv[], char** envp) {
 
-	char *port_name = NULL;
-	size_t size;
-	cout << "Enter port: ";
-	if (getline(&port_name, &size, stdin) == -1) {
-		cout << "Error reading from stdin\n";
-		return 1;
-	}
-
 	MPI::Init(argc, argv);
 
-	// char port_name[MPI_MAX_PORT_NAME];
-	// MPI::Lookup_name("server", MPI::INFO_NULL, port_name);
+	char port_name[MPI_MAX_PORT_NAME];
+	MPI::Lookup_name("server", MPI::INFO_NULL, port_name);
+
 	cout << "Using port " << port_name << "\n";
 
 	MPI::Intracomm server = MPI::COMM_WORLD.Connect(port_name,
